6397af8e9f81c0b70727f55a38dd080798123c07
Yusuf Bakhtiar <cakyus@gmail.com>
2017-12-21 09:36:13 +0700

    Fix #4

--- Pengujian-Referensi/2.Akun/merge_akun6_span_sakti.sql
+++ Pengujian-Referensi/2.Akun/merge_akun6_span_sakti.sql
@@ -34,7 +34,7 @@ MERGE INTO ADM_R_AKUN sakti USING (
 		) span
 	FULL OUTER JOIN ADM_R_AKUN sakti
 		ON sakti.kode = span.kode
-) span ON span.kode = sakti.kode
+) span ON (span.kode = sakti.kode)
 WHEN matched THEN
 	UPDATE SET 
 		sakti.deskripsi     = span.deskripsi,
@@ -72,35 +72,35 @@ WHEN NOT matched THEN
 		0, -- version
 		'-', -- nama_akun
 		SUBSTR(span.kode,1,5), -- kelompok_akun
-		YEAR(SYSDATE), -- tahun_anggaran_kode
+		TO_CHAR(SYSDATE,'YYYY'), -- tahun_anggaran_kode
 		span.tipe_akun -- tipe_akun
-	); 
-
-COMMIT;
-    
-    select * from sakti_ut.ADM_R_AKUN ;
-    
-    SELECT ffv.flex_value,
-      CASE
-        WHEN ffv.enabled_flag='Y'
-        THEN 0
-        ELSE 1
-      END AS enabled_flag ,
-      ffvt.description,
-      SUBSTR( ffv.compiled_value_attributes, 5, 1 ) account_type
-    FROM fnd_flex_values@SPAN_ST ffv
-    LEFT JOIN fnd_flex_value_sets@SPAN_ST ffvs
-    ON ffv.flex_value_set_id = ffvs.flex_value_set_id
-    LEFT JOIN fnd_flex_values_tl@SPAN_ST ffvt
-    ON ffv.FLEX_VALUE_ID          = ffvt.FLEX_VALUE_ID
-    LEFT JOIN apps.fnd_flex_value_norm_hierarchy@SPAN_ST h
-    ON ffv.flex_value               = h.parent_flex_value
-    AND ffvs.flex_value_set_id      = h.flex_value_set_id
-    WHERE ffvs.FLEX_VALUE_SET_NAME='SPAN_AKUN'
-      --AND ffv.summary_flag          ='N'
-    AND ffvt.language ='IN'
-    AND ffv.flex_value NOT LIKE '%0'
-    AND (ffv.flex_value NOT LIKE 'B%'
-    AND ffv.flex_value NOT LIKE 'C%'
-    AND ffv.flex_value NOT LIKE 'T%') and SUBSTR(ffv.flex_value,1,5) not in (
-     select kode from sakti_ut.ADM_R_KELOMPOK_AKUN);
+	);
+--
+--COMMIT;
+--    
+--    select * from sakti_ut.ADM_R_AKUN ;
+--    
+--    SELECT ffv.flex_value,
+--      CASE
+--        WHEN ffv.enabled_flag='Y'
+--        THEN 0
+--        ELSE 1
+--      END AS enabled_flag ,
+--      ffvt.description,
+--      SUBSTR( ffv.compiled_value_attributes, 5, 1 ) account_type
+--    FROM fnd_flex_values@SPAN_ST ffv
+--    LEFT JOIN fnd_flex_value_sets@SPAN_ST ffvs
+--    ON ffv.flex_value_set_id = ffvs.flex_value_set_id
+--    LEFT JOIN fnd_flex_values_tl@SPAN_ST ffvt
+--    ON ffv.FLEX_VALUE_ID          = ffvt.FLEX_VALUE_ID
+--    LEFT JOIN apps.fnd_flex_value_norm_hierarchy@SPAN_ST h
+--    ON ffv.flex_value               = h.parent_flex_value
+--    AND ffvs.flex_value_set_id      = h.flex_value_set_id
+--    WHERE ffvs.FLEX_VALUE_SET_NAME='SPAN_AKUN'
+--      --AND ffv.summary_flag          ='N'
+--    AND ffvt.language ='IN'
+--    AND ffv.flex_value NOT LIKE '%0'
+--    AND (ffv.flex_value NOT LIKE 'B%'
+--    AND ffv.flex_value NOT LIKE 'C%'
+--    AND ffv.flex_value NOT LIKE 'T%') and SUBSTR(ffv.flex_value,1,5) not in (
+--     select kode from sakti_ut.ADM_R_KELOMPOK_AKUN);
