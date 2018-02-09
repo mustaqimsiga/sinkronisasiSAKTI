2118ec38ec037a134c920f4f374f4f0ec2780f31
barthi <barthi77@gmail.com>
2018-02-09 11:40:56 +0700

    update no_reksus di merge_register

--- Pengujian-Referensi/5.Dana/merge_register_span_sakti.sql
+++ Pengujian-Referensi/5.Dana/merge_register_span_sakti.sql
@@ -6,7 +6,8 @@ merge INTO sakti_ut.ADM_R_REGISTER arb USING
   nvl2(ffv.flex_value,ffv.no_nphln,arb.no_pln) no_nphln,
   nvl2(ffv.flex_value,ffv.nilai_phln,arb.pagu) nilai_phln,
   nvl2(ffv.flex_value,ffv.tgl_nphln,arb.tgl_pln) tgl_npln,
-  nvl2(ffv.flex_value,ffv.nod_currency,arb.kode_mata_uang) nod_currency
+  nvl2(ffv.flex_value,ffv.nod_currency,arb.kode_mata_uang) nod_currency,
+  nvl2(ffv.flex_value,ffv.no_reksus,arb.no_reksus) no_reksus
 FROM
   (SELECT register_no flex_value,
     CASE
@@ -21,8 +22,34 @@ FROM
     no_nphln,
     nilai_phln,
     tgl_nphln,
-    nod_currency
-  FROM apps.sppm_register_lender@SPAN_ST
+    nod_currency,
+    (SELECT bank_account_num
+    FROM
+      (SELECT u.bank_account_num,
+        u.attribute,
+        u.register
+      FROM ce_bank_accounts@SPAN_ST s unpivot ( register FOR attribute IN (attribute10, attribute11, attribute12, attribute13, attribute14, attribute15) ) u
+      WHERE u.register                                                 IN
+        (SELECT register
+        FROM
+          (SELECT register,
+            COUNT(*) jml
+          FROM
+            (SELECT u.bank_account_num,
+              u.attribute,
+              u.register
+            FROM ce_bank_accounts@SPAN_ST s unpivot ( register FOR attribute IN (attribute10, attribute11, attribute12, attribute13, attribute14, attribute15) ) u
+            WHERE u.end_date                                                 IS NULL
+            )
+          GROUP BY register
+          HAVING COUNT(*) = 1
+          )
+        )
+      AND u.end_date IS NULL
+      )
+    WHERE register=srl.register_no
+    ) no_reksus
+  FROM apps.sppm_register_lender@SPAN_ST srl
   ) ffv
 FULL OUTER JOIN sakti_ut.ADM_R_REGISTER arb
 ON arb.kode              =ffv.flex_value
@@ -36,6 +63,7 @@ WHEN matched THEN
     arb.pagu           =src.nilai_phln,
     arb.tgl_pln        =src.tgl_npln,
     arb.kode_mata_uang =src.nod_currency,
+    arb.no_reksus      =src.no_reksus,
     arb.modified_date  = sysdate WHEN NOT matched THEN
   INSERT
     (
@@ -49,7 +77,8 @@ WHEN matched THEN
       no_pln,
       pagu,
       tgl_pln,
-      kode_mata_uang
+      kode_mata_uang,
+      no_reksus
     )
     VALUES
     (
@@ -63,7 +92,7 @@ WHEN matched THEN
       NVL(src.no_nphln,'ZZZ'),
       src.nilai_phln,
       src.tgl_npln,
-      src.nod_currency
+      src.nod_currency,
+      src.no_reksus
     );
-    
   COMMIT;
