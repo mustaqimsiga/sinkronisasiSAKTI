28aaca18aea3acd9eb961275c7b14cf9031f4185
barthi <barthi77@gmail.com>
2020-08-12 17:55:17 +0700

    Update merge_fungsi_span_sakti.sql
    update filter FLEX_VALUE

--- Pengujian-Referensi/4.Output/merge_fungsi_span_sakti.sql
+++ Pengujian-Referensi/4.Output/merge_fungsi_span_sakti.sql
@@ -18,6 +18,7 @@ FROM
   WHERE ffvs.FLEX_VALUE_SET_NAME='SPAN_DFF_FUNGSI'
   AND ffv.summary_flag          ='N'
   AND ffvt.language             ='IN'
+  AND REGEXP_LIKE(FFV.FLEX_VALUE , '^[0-9]{2}$')
   ) ffv
 FULL OUTER JOIN sakti_app.ADM_R_FUNGSI arb
 ON arb.kode              =ffv.flex_value
